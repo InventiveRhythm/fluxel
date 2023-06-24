c7758fee6ef93943b480281aa3688f18f3b11973
Flux <flustixshinu@gmail.com>
2023-06-24 12:43:54 +0200

    code refactor

--- fluxel/API/Routes/Account/AccountUpdateRoute.cs
+++ fluxel/API/Routes/Account/AccountUpdateRoute.cs
@@ -28,133 +28,59 @@ public class AccountUpdateRoute : IApiRoute {
                 Message = "Unauthorized (invalid token)"
             };
         }
-        
-        if (req.ContentType == null) {
-            return new ApiResponse {
-                Status = 400,
-                Message = "Missing content type"
-            };
-        }
-        
-        var enc = req.ContentEncoding;
-        var boundaryBytes = enc.GetBytes(GetBoundary(req.ContentType));
-        var boundaryLen = boundaryBytes.Length;
-
-        const int maxLen = 4194304;
-        
-        var input = req.InputStream;
-        
-        var stream = new MemoryStream();
-        input.CopyTo(stream);
-        int startPos;
-        
-        // limit to 4MB
-        if (stream.Length > maxLen) {
-            return new ApiResponse {
-                Message = "Image too large",
-                Status = 400
-            };
-        }
-        
-        // Find start boundary
-        while (true)
-        {
-            if (stream.Length == 0)
-                throw new Exception("Start Boundaray Not Found");
-
-            startPos = IndexOf(stream.ToArray(), (int) stream.Length, boundaryBytes);
-            if (startPos >= 0)
-                break;
-
-            var temp = new MemoryStream();
-            stream.Position = stream.Length - boundaryLen;
-            stream.CopyTo(temp);
-            stream.Dispose();
-            stream = temp;
-        }
-        
-        for (var i = 0; i < 4; i++)
-        {
-            while (true)
-            {
-                if (stream.Length == 0) 
-                    throw new Exception("Preamble not Found.");
-                
-                startPos = IndexOf(stream, enc.GetBytes("\n")[0], startPos);
-                if (startPos >= 0)
-                {
-                    startPos++;
-                    break;
-                }
-            }
-        }
-        
-        var buffer = new byte[stream.Length - startPos];
-        stream.Position = startPos;
-        var read = stream.Read(buffer, 0, buffer.Length);
-
-        if (!buffer.IsImage()) {
-            return new ApiResponse {
-                Message = "Invalid image",
-                Status = 400
-            };
-        }
 
         switch (action) {
-            case "avatar":
-                Assets.WriteAsset(AssetType.Avatar, user.UserId, buffer);
+            case "avatar" or "banner":
+                if (req.ContentType == null) {
+                    return new ApiResponse {
+                        Status = 400,
+                        Message = "Missing content type"
+                    };
+                }
+
+                var stream = new MemoryStream();
+                req.InputStream.CopyTo(stream);
+        
+                // limit to 4MB
+                if (stream.Length > 4194304) {
+                    return new ApiResponse {
+                        Message = "Image too large",
+                        Status = 400
+                    };
+                }
+        
+                var buffer = StreamUtils.GetPostFile(req.ContentEncoding, req.ContentType, stream);
+
+                if (!buffer.IsImage()) {
+                    return new ApiResponse {
+                        Message = "Invalid image",
+                        Status = 400
+                    };
+                }
                 
-                return new ApiResponse {
-                    Message = "Avatar updated",
-                    Status = 200
-                };
-            
-            case "banner":
-                Assets.WriteAsset(AssetType.Banner, user.UserId, buffer);
+                switch (action) {
+                    case "avatar":
+                        Assets.WriteAsset(AssetType.Avatar, user.UserId, buffer);
                 
-                return new ApiResponse {
-                    Message = "Banner updated",
-                    Status = 200
-                };
+                        return new ApiResponse {
+                            Message = "Avatar updated",
+                            Status = 200
+                        };
+                    case "banner":
+                        Assets.WriteAsset(AssetType.Banner, user.UserId, buffer);
+                
+                        return new ApiResponse {
+                            Message = "Banner updated",
+                            Status = 200
+                        };
+                }
+
+                break;
         }
         
         return new ApiResponse {
             Message = "Invalid action",
             Status = 400
         };
-    }
-    
-    private static string GetBoundary(string ctype)
-    {
-        return "--" + ctype.Split(';')[1].Split('=')[1];
-    }
-    
-    private static int IndexOf(IReadOnlyList<byte> buffer, int len, IReadOnlyList<byte> boundaryBytes)
-    {
-        for (var i = 0; i <= len - boundaryBytes.Count; i++)
-        {
-            var match = true;
-            for (var j = 0; j < boundaryBytes.Count && match; j++)
-                match = buffer[i + j] == boundaryBytes[j];
-
-            if (match)
-                return i;
-        }
-
-        return -1;
-    }
-
-    private static int IndexOf(Stream stream, byte bytes, int start) {
-        var buffer = new byte[stream.Length];
-        stream.Position = 0;
-        var read = stream.Read(buffer, 0, (int) stream.Length);
-
-        for (var i = start; i < buffer.Length; i++) {
-            if (buffer[i] == bytes) {
-                return i;
-            }
-        }
-
-        return -1;
     }
 }
---
+++ fluxel/Utils/StreamUtils.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+
+namespace fluxel.Utils; 
+
+// ReSharper disable MustUseReturnValue
+public static class StreamUtils {
+    public static byte[] GetPostFile(Encoding enc, string type, MemoryStream stream) {
+        var boundaryBytes = enc.GetBytes(GetBoundary(type));
+        var boundaryLen = boundaryBytes.Length;
+        
+        int startPos;
+        
+        // Find start boundary
+        while (true)
+        {
+            if (stream.Length == 0)
+                throw new Exception("Start Boundaray Not Found");
+
+            startPos = IndexOf(stream.ToArray(), (int) stream.Length, boundaryBytes);
+            if (startPos >= 0)
+                break;
+
+            var temp = new MemoryStream();
+            stream.Position = stream.Length - boundaryLen;
+            stream.CopyTo(temp);
+            stream.Dispose();
+            stream = temp;
+        }
+        
+        for (var i = 0; i < 4; i++)
+        {
+            while (true)
+            {
+                if (stream.Length == 0) 
+                    throw new Exception("Preamble not Found.");
+                
+                startPos = IndexOf(stream, enc.GetBytes("\n")[0], startPos);
+                if (startPos < 0) continue;
+                
+                startPos++;
+                break;
+            }
+        }
+        
+        var buffer = new byte[stream.Length - startPos];
+        stream.Position = startPos;
+        stream.Read(buffer, 0, buffer.Length);
+
+        return buffer;
+    }
+    
+    private static string GetBoundary(string ctype)
+    {
+        return "--" + ctype.Split(';')[1].Split('=')[1];
+    }
+    
+    private static int IndexOf(IReadOnlyList<byte> buffer, int len, IReadOnlyList<byte> boundaryBytes)
+    {
+        for (var i = 0; i <= len - boundaryBytes.Count; i++)
+        {
+            var match = true;
+            for (var j = 0; j < boundaryBytes.Count && match; j++)
+                match = buffer[i + j] == boundaryBytes[j];
+
+            if (match)
+                return i;
+        }
+
+        return -1;
+    }
+
+    private static int IndexOf(Stream stream, byte bytes, int start) {
+        var buffer = new byte[stream.Length];
+        stream.Position = 0;
+        stream.Read(buffer, 0, (int) stream.Length);
+
+        for (var i = start; i < buffer.Length; i++) {
+            if (buffer[i] == bytes) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
