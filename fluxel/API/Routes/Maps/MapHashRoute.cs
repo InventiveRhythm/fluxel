3679a9d38e77f2599fd2417ac3045ebb15886163
Flux <flustixshinu@gmail.com>
2023-09-07 20:24:19 +0200

    hash map searching

---
+++ fluxel/API/Routes/Maps/MapHashRoute.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using fluxel.API.Components;
+using fluxel.Components.Maps;
+using fluxel.Constants;
+using fluxel.Database;
+
+namespace fluxel.API.Routes.Maps;
+
+public class MapHashRoute : IApiRoute {
+    public string Path => "/map/hash/:hash";
+    public string Method => "GET";
+
+    public ApiResponse Handle(HttpListenerRequest req, HttpListenerResponse res, Dictionary<string, string> parameters) {
+        var hash = parameters["hash"];
+
+        return RealmAccess.Run(realm => {
+            var map = realm.All<Map>().FirstOrDefault(m => m.Hash == hash);
+
+            if (map == null) {
+                return new ApiResponse {
+                    Status = HttpStatusCode.NotFound,
+                    Message = ResponseStrings.MapNotFound
+                };
+            }
+
+            return new ApiResponse {
+                Data = map
+            };
+        });
+    }
+}
