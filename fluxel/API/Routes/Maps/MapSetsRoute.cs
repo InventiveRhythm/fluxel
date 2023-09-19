cf82a8ea2875583f6cfaf257a6602b77995f717f
Flux <flustixshinu@gmail.com>
2023-09-19 04:14:40 +0200

    mapsets list

---
+++ fluxel/API/Routes/Maps/MapSetsRoute.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using fluxel.API.Components;
+using fluxel.Components.Maps;
+using fluxel.Database;
+
+namespace fluxel.API.Routes.Maps;
+
+public class MapSetsRoute : IApiRoute
+{
+    public string Path => "/mapsets";
+    public string Method => "GET";
+
+    public ApiResponse? Handle(HttpListenerRequest req, HttpListenerResponse res, Dictionary<string, string> parameters)
+    {
+        return new ApiResponse
+        {
+            Data = RealmAccess.Run(realm => realm.All<MapSet>())
+        };
+    }
+}
