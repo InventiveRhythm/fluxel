d37ebf90d174f069bf0b3a0a76364b946f41d993
Flux <flustixshinu@gmail.com>
2023-09-30 21:38:28 +0200

    Add server messages

---
+++ fluxel/Components/ServerMessage.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace fluxel.Components;
+
+public class ServerMessage
+{
+    /// <summary>
+    /// The text to display.
+    /// </summary>
+    [JsonProperty("text")]
+    public string Text { get; init; } = "";
+
+    /// <summary>
+    /// The subtext to display.
+    /// Only used when <see cref="ServerMessage.NonDisruptive"/> is <see langword="false"/>.
+    /// </summary>
+    [JsonProperty("sub_text")]
+    public string SubText { get; init; } = "";
+
+    /// <summary>
+    /// Whether this message should be displayed as a "SmallFloatingTextNotification".
+    /// </summary>
+    [JsonProperty("non_disruptive")]
+    public bool NonDisruptive { get; init; }
+}
