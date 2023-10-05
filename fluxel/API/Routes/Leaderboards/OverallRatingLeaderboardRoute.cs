68da371e91059f96af02a2751d3c1c93a5d6f0fb
Flux <flustixshinu@gmail.com>
2023-10-05 20:51:16 +0200

    overall rating leaderboard

---
+++ fluxel/API/Routes/Leaderboards/OverallRatingLeaderboardRoute.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using fluxel.API.Components;
+using fluxel.Components.Users;
+using fluxel.Database;
+
+namespace fluxel.API.Routes.Leaderboards;
+
+public class OverallRatingLeaderboardRoute : IApiRoute
+{
+    public string Path => "/leaderboards/overall";
+    public string Method => "GET";
+
+    public ApiResponse? Handle(HttpListenerRequest req, HttpListenerResponse res, Dictionary<string, string> parameters)
+    {
+        return RealmAccess.Run(realm => new ApiResponse
+        {
+            Data = realm.All<User>().OrderByDescending(u => u.OverallRating).Take(100).ToList()
+        });
+    }
+}
